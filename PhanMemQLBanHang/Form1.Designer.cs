dfa8483893a4f7ff621abc7f203505226055ac62
Tien <tiendohuu363@gmail.com>
2026-04-06 21:06:43 +0700

    test

--- PhanMemQLBanHang/Form1.Designer.cs
+++ PhanMemQLBanHang/Form1.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(290, 194);
+            this.label1.Location = new System.Drawing.Point(249, 178);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(186, 54);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nhóm 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(482, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 54);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhóm 2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
